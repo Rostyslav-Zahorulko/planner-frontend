06d2081d32be061159eeaf385d82de64ee345bf7
mdcreator <63508140+mdcreator@users.noreply.github.com>
2021-06-29 01:40:37 +0300

    added markup and styles of Header

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,6 +1,17 @@
-import Header from '../Header';
-// import s from './AppBar.module.css';
+import { Link } from 'react-router-dom';
+import UserMenu from '../UserMenu';
 
-export default function AppBar() {
-  return <Header />;
+import logo from '../../images/logo.svg';
+import s from './AppBar.module.css';
+
+export default function AppBar({ isLoggedIn }) {
+  return (
+    <header className={s.header}>
+      <Link to="/">
+        <img src={logo} className={s.logo} alt={logo}></img>
+      </Link>
+
+      {isLoggedIn ? <UserMenu /> : null}
+    </header>
+  );
 }
