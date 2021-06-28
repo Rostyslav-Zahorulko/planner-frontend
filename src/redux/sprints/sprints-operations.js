7629ee5c2d68ed2e14186fd5e432565b5352100d
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2021-06-28 12:42:45 +0300

    add sprints operation

---
+++ src/redux/sprints/sprints-operations.js
@@ -0,0 +1,85 @@
+import axios from 'axios';
+import { sprintsActions } from '../actions';
+
+const {
+  getSprintsRequest,
+  getSprintsSuccess,
+  getSprintsError,
+  addSprintRequest,
+  addSprintSuccess,
+  addSprintError,
+  deleteSprintRequest,
+  deleteSprintSuccess,
+  deleteSprintError,
+  editSprintTitleRequest,
+  editSprintTitleSuccess,
+  editSprintTitleError,
+} = sprintsActions;
+
+const getSprints = projectId => async dispatch => {
+  dispatch(getSprintsRequest());
+
+  try {
+    const { data } = await axios.get(`/projects/${projectId}`);
+
+    dispatch(getSprintsSuccess(data));
+  } catch ({ message }) {
+    dispatch(getSprintsError(message));
+  }
+};
+
+const addSprint =
+  ({ projectId, sprint }) =>
+  async dispatch => {
+    dispatch(addSprintRequest());
+
+    try {
+      const { data } = axios.post(`/projects/${projectId}`, sprint);
+
+      dispatch(addSprintSuccess(data));
+    } catch ({ message }) {
+      dispatch(addSprintError(message));
+    }
+  };
+
+const deleteSprint =
+  ({ projectId, sprintId }) =>
+  async dispatch => {
+    dispatch(deleteSprintRequest());
+
+    try {
+      axios.delete(`/projects/${projectId}/${sprintId}`);
+
+      dispatch(deleteSprintSuccess(sprintId));
+    } catch ({ message }) {
+      dispatch(deleteSprintError(message));
+    }
+  };
+
+const editSprintTitle =
+  ({ projectId, sprintId, title }) =>
+  async dispatch => {
+    const update = { title };
+
+    dispatch(editSprintTitleRequest());
+
+    try {
+      const { data } = axios.patch(
+        `/projects/${projectId}/${sprintId}`,
+        update,
+      );
+
+      dispatch(editSprintTitleSuccess(data));
+    } catch ({ message }) {
+      dispatch(editSprintTitleError(message));
+    }
+  };
+
+const sprintsOperations = {
+  getSprints,
+  addSprint,
+  deleteSprint,
+  editSprintTitle,
+};
+
+export default sprintsOperations;
