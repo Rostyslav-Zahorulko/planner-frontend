0d775d04dfefacdb16e58238c096ce0bb3a91739
mdcreator <63508140+mdcreator@users.noreply.github.com>
2021-06-28 23:55:58 +0300

    fix Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+
 import Container from './components/Container';
 import AppBar from './components/AppBar/AppBar';
 
 export default function App() {
   return (
     <Container>
-      <Router>
-        <AppBar />
-      </Router>
+      <AppBar />
     </Container>
   );
 }
