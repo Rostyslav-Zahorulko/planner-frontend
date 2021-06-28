558b6b29a842ca100d7b801a9a88e7ca5a6e7477
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2021-06-28 15:07:58 +0300

    add tasks reducer

--- src/redux/sprints/index.js
+++ src/redux/sprints/index.js
@@ -1,2 +1,3 @@
 export { default as sprintsActions } from './sprints-actions';
 export { default as sprintsOperations } from './sprints-operations';
+export { default as sprintsReducer } from './sprints-reducer';
