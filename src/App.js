2bce9ca0d96e4f64750ca5ada1dac2fd9f8ba686
mdcreator <63508140+mdcreator@users.noreply.github.com>
2021-06-28 03:10:06 +0300

    render markup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import Container from "./components/Container";
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Container from './components/Container';
+import AppBar from './components/AppBar/AppBar';
 
 export default function App() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Router>
+        <AppBar />
+      </Router>
+    </Container>
+  );
 }
---
+++ src/components/AppBar/AppBar.js
@@ -0,0 +1,6 @@
+import Header from '../Header';
+// import s from './AppBar.module.css';
+
+export default function AppBar() {
+  return <Header />;
+}
