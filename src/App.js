df51835061f70cfa64bcda667c716c4f26d9de0f
Natalia2525 <65954006+Natalia2525@users.noreply.github.com>
2021-06-29 16:52:37 +0300

    add tasks page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Container from './components/Container';
-import RegisterPage from './pages/RegisterPage';
-import LoginPage from './pages/LoginPage';
-import ProjectsPage from './pages/ProjectsPage';
+// import RegisterPage from './pages/RegisterPage';
+import TasksPage from './pages/TasksPage/TasksPage';
+// import LoginPage from './pages/LoginPage';
+// import ProjectsPage from './pages/ProjectsPage';
 
 export default function App() {
   return (
     <Container>
-      {<RegisterPage></RegisterPage>}
+      {/* {<RegisterPage></RegisterPage>} */}
       {/* {<LoginPage></LoginPage>} */}
       {/* <ProjectsPage></ProjectsPage> */}
+      <TasksPage />
     </Container>
   );
 }
---
+++ src/pages/TasksPage/TasksPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import st from './TasksPage.module.css';
+import tasks from '../../data/tasks.json';
+import TasksList from '../../components/TasksList';
+import SprintHeader from '../../components/SprintHeader';
+
+import AddButton from '../../components/AddButton';
+
+export default function TasksPage() {
+  return (
+    <>
+      <div className={st.date}>
+        <span className={st.date_left}>
+        </span>
+        <p className={st.date_sprint}>
+          1/
+          <span>10 </span>
+          <span className={st.date_right}> </span>
+          <span>08.08.2020</span>
+        </p>
+      </div>
+      <div className={st.header}>
+        <div className={st.title_wrapper}>
+          <h1 className={st.title}>Sprint 1</h1>
+          <button className={st.button_edit}></button>
+        </div>
+        <div className={st.button_wrapper}>
+          <AddButton />
+          <p className={st.name_button}>Створити задачу</p>
+        </div>
+      </div>
+      <SprintHeader />
+      <TasksList tasks={tasks} />
+    </>
+  );
+}
