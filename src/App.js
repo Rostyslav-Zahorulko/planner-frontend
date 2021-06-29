99230b91283861e8fab87b0a4cce8855b5f77943
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2021-06-29 14:53:11 +0300

    add lazy load

--- src/App.js
+++ src/App.js
@@ -1,43 +1,66 @@
+import { Suspense, lazy } from 'react';
 import { Switch } from 'react-router-dom';
 
-import RegisterPage from './pages/RegisterPage';
-import LoginPage from './pages/LoginPage';
-import ProjectsPage from './pages/ProjectsPage';
-import SprintsPage from './pages/SprintsPage';
-import TasksPage from './pages/TasksPage';
-
+import Container from './components/Container';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
-import Container from './components/Container';
 
 import routes from './routes';
 
 const { register, login, projects, sprints, tasks } = routes;
 
+const RegisterPage = lazy(() =>
+  import(
+    './pages/RegisterPage/RegisterPage' /* webpackChunkName: "register-page" */
+  ),
+);
+
+const LoginPage = lazy(() =>
+  import('./pages/LoginPage/LoginPage' /* webpackChunkName: "login-page" */),
+);
+
+const ProjectsPage = lazy(() =>
+  import(
+    './pages/ProjectsPage/ProjectsPage' /* webpackChunkName: "projects-page" */
+  ),
+);
+
+const SprintsPage = lazy(() =>
+  import(
+    './pages/SprintsPage/SprintsPage' /* webpackChunkName: "sprints-page" */
+  ),
+);
+
+const TasksPage = lazy(() =>
+  import('./pages/TasksPage/TasksPage' /* webpackChunkName: "tasks-page" */),
+);
+
 export default function App() {
   return (
     <Container>
-      <Switch>
-        <PublicRoute exact path={register} restricted redirectTo={projects}>
-          <RegisterPage />
-        </PublicRoute>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <PublicRoute exact path={register} restricted redirectTo={projects}>
+            <RegisterPage />
+          </PublicRoute>
 
-        <PublicRoute path={login} restricted redirectTo={projects}>
-          <LoginPage />
-        </PublicRoute>
+          <PublicRoute path={login} restricted redirectTo={projects}>
+            <LoginPage />
+          </PublicRoute>
 
-        <PrivateRoute exact path={projects} redirectTo={login}>
-          <ProjectsPage />
-        </PrivateRoute>
+          <PrivateRoute exact path={projects} redirectTo={login}>
+            <ProjectsPage />
+          </PrivateRoute>
 
-        <PrivateRoute exact path={sprints} redirectTo={login}>
-          <SprintsPage />
-        </PrivateRoute>
+          <PrivateRoute exact path={sprints} redirectTo={login}>
+            <SprintsPage />
+          </PrivateRoute>
 
-        <PrivateRoute path={tasks} redirectTo={login}>
-          <TasksPage />
-        </PrivateRoute>
-      </Switch>
+          <PrivateRoute path={tasks} redirectTo={login}>
+            <TasksPage />
+          </PrivateRoute>
+        </Switch>
+      </Suspense>
     </Container>
   );
 }
