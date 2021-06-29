8f4ff83a41157e230974cb97ee3e3e2398677026
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2021-06-29 14:23:39 +0300

    add routing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { store, persistor } from './redux/store';
 import 'modern-normalize/modern-normalize.css';
@@ -11,7 +12,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
