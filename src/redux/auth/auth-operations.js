e77d47d487e395b2d4b999cd4beb3b472573fb82
Zoe Baletska <zoya.miori@gmail.com>
2021-06-30 22:03:21 +0300

    fixed getProjects path

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { authActions } from '../auth';
 
 // ССЫЛКА НА ОПУБЛИКоВАНЫЙ БЭК
-axios.defaults.baseURL = 'https://dreamteam-planner-api.herokuapp.com/';
+// axios.defaults.baseURL = 'https://dreamteam-planner-api.herokuapp.com/';
 
 // ССЫЛКА НА ЛОКАЛЬНО ЗАПУЩЕННЫЙ БЭК ДЛЯ РАЗРАБОТКИ
-// axios.defaults.baseURL = 'http://localhost:3000/api/';
+axios.defaults.baseURL = 'http://localhost:3000/api/';
 
 const token = {
   set(token) {
