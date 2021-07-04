300867cc1cbbea2a3bc1dfd78df1c71026dae287
Oksana Cherepanova <oksa-lera@ukr.net>
2021-07-04 21:32:04 +0300

    dateFormat

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -6,7 +6,7 @@ const Calendar = () => {
 
   return (
     <DatePicker
-      dateFormat="dd.MM.yyyy"
+      dateFormat="yyyy-MM-dd"
       selected={startDate}
       onChange={date => setStartDate(date)}
       startDate={startDate}
