e1d61a6a3f9ba296ee1920c4e1e8f3b10522cb8e
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2021-07-06 00:00:39 +0300

    add create project form on projects page & sprints page

--- src/components/CreatePojectForm/CreateProjectForm.js
+++ src/components/CreatePojectForm/CreateProjectForm.js
@@ -1,204 +1,90 @@
-import React from 'react';
+import { useState, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import s from './CreateProjectForm.module.css';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import addProject from '../../redux/projects/projects-operations';
 
-const formSchema = Yup.object().shape({
-  name: Yup.string('Enter a name')
-    .min(3, 'Too short!')
-    .required('* Project Name is a required field'),
-  description: Yup.string('Enter a name')
-    .min(4, 'Too short!')
-    .required('* Description is a required field'),
-});
+import FormButtons from '../FormButtons';
+
+import styles from './CreateProjectForm.module.css';
+
+import { projectsOperations } from '../../redux/projects';
+
+const { addProject } = projectsOperations;
 
 const CreateProjectForm = ({ onClose }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
   const dispatch = useDispatch();
 
+  const handleInputChange = useCallback(({ target: { name, value } }) => {
+    switch (name) {
+      case 'title':
+        setTitle(value);
+        break;
+
+      case 'description':
+        setDescription(value);
+        break;
+
+      default:
+        console.warn(`Тип поля ${name} не обрабатывается`);
+    }
+  }, []);
+
+  const handleFormSubmit = useCallback(
+    event => {
+      event.preventDefault();
+
+      const project = {
+        title,
+        description,
+      };
+
+      console.log(project);
+
+      dispatch(addProject(project));
+
+      setTitle('');
+      setDescription('');
+      onClose();
+    },
+    [title, description, onClose, dispatch],
+  );
+
   return (
-    <Formik
-      initialValues={{
-        name: '',
-        description: '',
-      }}
-      validationSchema={formSchema}
-      onSubmit={values => {
-        addProject(values);
-        dispatch(addProject(values));
-        onClose();
-      }}
+    <form
+      className={styles.ProjectForm}
+      autoComplete="off"
+      onSubmit={handleFormSubmit}
     >
-      <Form
-        className={s.ProjectForm}
-        //   onChange={handleChange}
-      >
-        <div className={s.FormField}>
-          <Field
-            className={s.FormInput}
-            type="text"
-            name="name"
-            placeholder=" "
-          />
-
-          <label className={s.FormLable}>Project name</label>
-        </div>
-        <ErrorMessage className={s.ErrorInput} component="span" name="name" />
-        <div className={s.FormField}>
-          <Field
-            className={s.FormInput}
-            type="text"
-            name="description"
-            placeholder=" "
-          />
-
-          <label className={s.FormLable}>Description</label>
-        </div>
-
-        <ErrorMessage
-          className={s.ErrorInput}
-          component="span"
-          name="description"
+      <div className={styles.FormField}>
+        <input
+          className={styles.FormInput}
+          type="text"
+          name="title"
+          value={title}
+          required
+          placeholder=" "
+          onChange={handleInputChange}
         />
-      </Form>
-    </Formik>
+        <label className={styles.FormLable}>Project name</label>
+      </div>
+
+      <div className={styles.FormField}>
+        <input
+          className={styles.FormInput}
+          type="text"
+          name="description"
+          value={description}
+          required
+          placeholder=" "
+          onChange={handleInputChange}
+        />
+        <label className={styles.FormLable}>Description</label>
+      </div>
+
+      <FormButtons onClose={onClose} />
+    </form>
   );
 };
 
 export default CreateProjectForm;
-
-//v.2
-
-// import { useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import s from './CreateProjectForm.module.css';
-
-// import { Formik, Form, Field, ErrorMessage } from 'formik';
-// import * as Yup from 'yup';
-// import { projectsOperations, projectsSelectors } from '../../redux/projects';
-// import { errorSelectors } from '../../redux/error';
-
-// const FormSchema = Yup.object().shape({
-//   name: Yup.string('Enter a name')
-//     .min(3, 'Too short!')
-//     .required('* Project Name is a required field'),
-//   description: Yup.string('Enter a name')
-//     .min(4, 'Too short!')
-//     .required('* Description is a required field'),
-// });
-
-// const createErrorMessage = error => {
-//   if (error.includes('400')) return 'Bad request. Please try again';
-//   if (error.includes('401')) return 'User not authorized';
-//   return 'Unknown error. Please try again';
-// };
-
-// const CreateProjectForm = ({ onSave }) => {
-//   const [name, setName] = useState('');
-//   const [description, setDescription] = useState('');
-
-//   const handleChange = e => {
-//     const { name, value } = e.target;
-//     switch (name) {
-//       case 'name':
-//         return setName(value);
-//       case 'description':
-//         return setDescription(value);
-//       default:
-//         return;
-//     }
-//   };
-
-//   const projects = useSelector(projectsSelectors.getProjects);
-//   const dispatch = useDispatch();
-
-//   const errorFromState = useSelector(errorSelectors);
-
-//   let errorMessage = errorFromState ? createErrorMessage(errorFromState) : null;
-
-//   const handleSubmit = (
-//     { name, description },
-//     { resetForm, setSubmitting },
-//   ) => {
-//     if (projects.some(project => project.name === name)) {
-//       return (errorMessage = `Name "${name}" already exists, please enter another name.`);
-//     } else dispatch(projectsOperations.addProject(name, description));
-
-//     setSubmitting(false);
-//     resetForm();
-
-//     // console.log(name, description);
-//     onSave();
-//   };
-
-//   return (
-//     <Formik
-//       initialValues={{
-//         name: '',
-//         description: '',
-//       }}
-//       validationSchema={FormSchema}
-//       //   onCreate={values => {
-//       // addProject(values);
-//       // dispatch(addProject(values));
-//       // console.log(values);
-//       // onClose();
-//       //   }}
-//       onSubmit={handleSubmit}
-//     >
-//       {({ errors, touched }) => (
-//         <Form className={s.ProjectForm} onChange={handleChange}>
-//           <div className={s.FormField}>
-//             <Field
-//               className={`${s.FormInput} ${
-//                 touched.name && errors.name && s.ErrorInput
-//               }`}
-//               type="text"
-//               name="name"
-//               placeholder=" "
-//               // value={prName}
-//               // onChange={setPrName}
-//             />
-
-//             <label className={s.FormLable}>Project name</label>
-//             {touched.name && errors.name && (
-//               <div className={s.error}>{errors.name}</div>
-//             )}
-//           </div>
-//           {/* <ErrorMessage className={s.ErrorName} component="span" name="name" /> */}
-//           <div className={s.FormField}>
-//             <Field
-//               className={`${s.FormInput} ${
-//                 touched.description && errors.description && s.ErrorInput
-//               }`}
-//               type="text"
-//               name="description"
-//               placeholder=" "
-//               // value={prDesc}
-//               // onChange={setPrDesc}
-//             />
-
-//             <label className={s.FormLable}>Description</label>
-//             {touched.description && errors.description && (
-//               <div className={s.error}>{errors.description}</div>
-//             )}
-//           </div>
-
-//           {/* <ErrorMessage
-//             className={s.ErrorDescription}
-//             component="span"
-//             name="description"
-//           /> */}
-//           <div className={s.wr}>
-//             {errorMessage ? (
-//               <div className={s.errorMessage}>{errorMessage}</div>
-//             ) : null}
-//           </div>
-//         </Form>
-//       )}
-//     </Formik>
-//   );
-// };
-
-// export default CreateProjectForm;
--- src/pages/ProjectsPage/ProjectsPage.js
+++ src/pages/ProjectsPage/ProjectsPage.js
@@ -1,64 +1,62 @@
 // Libraries
-import React, { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 // Components
 import ProjectsList from '../../components/ProjectsList';
 import AddButton from '../../components/AddButton';
 import Modal from '../../components/Modal';
+import CreateProjectForm from '../../components/CreatePojectForm';
+
+// Styles
+import s from './ProjectsPage.module.css';
 
 // Redux
 import { projectsOperations } from '../../redux/projects';
 import { projectsSelectors } from '../../redux/projects';
 
-// Styles
-import s from './ProjectsPage.module.css';
-
-// БАЗА ДЛЯ ПРОВЕРКИ (ЕСЛИ НЕ ЗАПУЩЕН БЭК)
-// import projects from '../../data/projects.json';
-
-// const { addProject } = projectsOperations;
-const { getProjectsItems } = projectsSelectors;
-
-export default function ProjectsPage() {
-  const [isShown, setIsShown] = useState(false);
-
-  const toggleModal = useCallback(() => {
-    setIsShown(prevIsShown => !prevIsShown);
-  }, []);
+const ProjectsPage = () => {
+  const { getProjects } = projectsOperations;
+  const { getProjectsItems } = projectsSelectors;
 
   const dispatch = useDispatch();
-
   const projects = useSelector(getProjectsItems);
 
-  // TODO: СДЕЛАТЬ, КОГДА БУДЕТ МОДАЛКА
-  // const onAddProject = value => dispatch(addProject(value));
+  const [isCreateProjectModalShown, setCreateProjectModalIsShown] =
+    useState(false);
+
+  /*Create project*/
+  const toggleCreateProjectModal = useCallback(() => {
+    setCreateProjectModalIsShown(prevValue => !prevValue);
+  }, []);
 
   useEffect(() => {
-    dispatch(projectsOperations.getProjects());
-  }, [dispatch]);
+    dispatch(getProjects());
+  }, [dispatch, getProjects]);
 
   return (
-    <>
-      <div className={s.container}>
-        <div className={s.headbar}>
-          <h1 className={s.title}>Projects</h1>
-          <div className={s.btnWrapper}>
-            <AddButton onOpen={toggleModal} />
-            <p className={s.btnText}>Create a project</p>
-          </div>
+    <div className={s.container}>
+      <div className={s.headbar}>
+        <h1 className={s.title}>Projects</h1>
+        <div className={s.btnWrapper}>
+          <AddButton onOpen={toggleCreateProjectModal} />
+          <p className={s.btnText}>Create a project</p>
         </div>
-        {projects.length > 0 ? (
-          <ProjectsList projects={projects} />
-        ) : (
-          <div className={s.noProjects}>No projects added yet</div>
-        )}
-        {isShown && (
-          <Modal title={'Creating a project'} onClose={toggleModal}>
-            <div>Тут повинен бути компонент форми для створення проекту</div>
-          </Modal>
-        )}
       </div>
-    </>
+
+      {projects.length > 0 ? (
+        <ProjectsList projects={projects} />
+      ) : (
+        <div className={s.noProjects}>No projects added yet</div>
+      )}
+
+      {isCreateProjectModalShown && (
+        <Modal title={'Creating a project'} onClose={toggleCreateProjectModal}>
+          <CreateProjectForm onClose={toggleCreateProjectModal} />
+        </Modal>
+      )}
+    </div>
   );
-}
+};
+
+export default ProjectsPage;
