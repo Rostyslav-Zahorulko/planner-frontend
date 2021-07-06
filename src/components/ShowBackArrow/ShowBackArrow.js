11accba89170b828d850c915689d5046ba38a4a2
mdcreator <63508140+mdcreator@users.noreply.github.com>
2021-07-06 23:45:51 +0300

    added fonctionality for "back to" buttons on sidebars

--- src/components/ShowBackArrow/ShowBackArrow.js
+++ src/components/ShowBackArrow/ShowBackArrow.js
@@ -1,6 +1,23 @@
+import PropTypes from 'prop-types';
 import styles from './ShowBackArrow.module.css';
 
-const ShowBackArrow = ({ goBackTo }) => (
-  <p className={styles.show_back}>Show {goBackTo}</p>
+// const ShowBackArrow = ({ goBackTo }) => (
+//   <p className={styles.show_back}>Show {goBackTo}</p>
+// );
+
+const ShowBackArrow = ({ goBackTo, onClick, status = false }) => (
+  <button
+    type="button"
+    className={styles.show_back}
+    onClick={onClick}
+    disabled={status}
+  >
+    <p className={styles.text}>Show {goBackTo}</p>
+  </button>
 );
 export default ShowBackArrow;
+
+ShowBackArrow.propTypes = {
+  onClick: PropTypes.func,
+  status: PropTypes.bool,
+};
--- src/components/SidebarForReuse/SidebarForReuse.js
+++ src/components/SidebarForReuse/SidebarForReuse.js
@@ -1,13 +1,21 @@
 import ShowBackArrow from '../ShowBackArrow';
 import SidebarAddButton from '../SidebarAddButton';
+import { useHistory, useLocation } from 'react-router-dom';
 // import FormCreateSprint from '../FormCreateSprint';
 
 import styles from './SidebarForReuse.module.css';
 
 const SidebarForReuse = ({ children, goBackTo }) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const handleGoBack = () => {
+    history.push(location?.state?.from?.location ?? '/projects/sprints');
+  };
+
   return (
     <div className={styles.sidebar}>
-      <ShowBackArrow goBackTo={goBackTo} />
+      <ShowBackArrow goBackTo={goBackTo} onClick={handleGoBack} />
       {children}
       <SidebarAddButton children={'Create a sprint'}>
         {/* <FormCreateSprint /> */}
