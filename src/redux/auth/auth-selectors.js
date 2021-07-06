6d897085a23041fb895fec8a5dc39d03b78e40b3
Rostyslav Zahorulko <66204377+Rostyslav-Zahorulko@users.noreply.github.com>
2021-07-06 20:00:40 +0300

    Update auth-selectors.js

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,10 +1,9 @@
 const getIsLoggedIn = state => state.auth.isLoggedIn;
 const getEmail = state => state.auth.user.email;
-//const getUserEmail = state => state.auth.users//
+
 const authSelectors = {
   getIsLoggedIn,
   getEmail,
-  // getUserEmail,
 };
 
 export default authSelectors;
