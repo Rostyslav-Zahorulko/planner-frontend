2a92e2935d9376d491775ee28c5576f943c9c9d0
Rostyslav Zahorulko <66204377+Rostyslav-Zahorulko@users.noreply.github.com>
2021-07-06 19:55:04 +0300

    Update FormAddPeople.js

--- src/components/FormAddPeople/FormAddPeople.js
+++ src/components/FormAddPeople/FormAddPeople.js
@@ -1,24 +1,37 @@
 import { useState, useCallback } from 'react';
-//import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import EmailPeopleList from '../EmailPeopleList';
 import FormButtons from '../FormButtons';
 
 import styles from './FormAddPeople.module.css';
-//Enter e-mail
+
+import { projectsOperations } from '../../redux/projects';
+
+// TODO: написати операцію
+// const { addUserInProject } = projectsOperations;
+
 const FormAddPeople = ({ onClose, users }) => {
   const [email, setEmail] = useState('');
 
-  //const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  const handleInputChange = e => setEmail(e.target.value);
+  const handleInputChange = useCallback(e => {
+    setEmail(e.target.value)
+  }, [])
+  
   const handleFormSubmit = useCallback(e => {
     e.preventDefault();
 
     const user = { email };
 
-    // dispatch(getUserEmail({ users, email }));
-  });
+    // dispatch(addUserInProject(user));
+    
+    setEmail('');
+    
+    onClose();
+  }, [email, onClose, dispatch]);
+      
   return (
     <form className={styles.wrap} onSubmit={handleFormSubmit}>
       <label className={styles.label}>
@@ -28,10 +41,10 @@ const FormAddPeople = ({ onClose, users }) => {
           onChange={handleInputChange}
           value={email}
           type="email"
-          name="mail"
+          name="email"
           required
         />
-        <span className={styles.headline}>The name of the sprint</span>
+        <span className={styles.headline}>Enter e-mail</span>
       </label>
       <p>Added users:</p>
       <EmailPeopleList users={users} />
@@ -39,4 +52,5 @@ const FormAddPeople = ({ onClose, users }) => {
     </form>
   );
 };
+
 export default FormAddPeople;
