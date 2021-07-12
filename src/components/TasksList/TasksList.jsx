38a9a7629ee0a90ecb0a98aa0ff1572491c8ff7a
Oksana Cherepanova <oksa-lera@ukr.net>
2021-07-12 21:53:58 +0300

    add styles task

--- src/components/TasksList/TasksList.jsx
+++ src/components/TasksList/TasksList.jsx
@@ -1,50 +1,31 @@
 import st from './TasksList.module.css';
 import TaskItem from '../TaskItem';
-import TaskItemCard from '../TaskItemCard';
-import Media from 'react-media';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+//import TaskItemCard from '../TaskItemCard';
+//import Media from 'react-media';
 
 const TasksList = ({ visibleTasks, projectId, sprintId }) => {
   return (
-    <Media queries={{ big: { minWidth: 1280 } }}>
-      {matches =>
-        matches.big ? (
-          <>
-            <ul className={st.task_list}>
-              {visibleTasks.map(({ id, title, plannedHours, totalHours }) => (
-                <TaskItem
-                  key={id}
-                  title={title}
-                  plannedHours={plannedHours}
-                  totalHours={totalHours}
-                  projectId={projectId}
-                  sprintId={sprintId}
-                  taskId={id}
-                />
-              ))}
-            </ul>
-          </>
-        ) : (
-          <>
-            <ul className={st.task_list}>
-              {visibleTasks.map(
-                ({ id, title, plannedHours, hoursPerDay, totalHours }) => (
-                  <TaskItemCard
-                    key={id}
-                    title={title}
-                    plannedHours={plannedHours}
-                    hoursPerDay={hoursPerDay}
-                    totalHours={totalHours}
-                    projectId={projectId}
-                    sprintId={sprintId}
-                    taskId={id}
-                  />
-                ),
-              )}
-            </ul>
-          </>
-        )
-      }
-    </Media>
+    <TransitionGroup component="ul" className={st.task_list}>
+      {visibleTasks.map(({ id, title, plannedHours, totalHours }) => (
+        <CSSTransition
+          className={st.task}
+          timeout={800}
+          mountOnEnter
+          unmountOnExit
+          key={id}
+        >
+          <TaskItem
+            title={title}
+            plannedHours={plannedHours}
+            totalHours={totalHours}
+            projectId={projectId}
+            sprintId={sprintId}
+            taskId={id}
+          />
+        </CSSTransition>
+      ))}
+    </TransitionGroup>
   );
 };
 
